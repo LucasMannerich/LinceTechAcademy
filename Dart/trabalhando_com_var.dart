18255f35601406babda66d808985cd57c2e850c2
LucasMannerich <lucasmannerich@gmail.com>
2024-03-20 10:40:46 -0300

    Aprendendo variáveis no dart

---
+++ Dart/trabalhando_com_var.dart
@@ -0,0 +1,10 @@
+void main() {
+  //var nomeCompleto = 'Lucas Mannerich';
+  //Object nomeCompleto = 'Lucas Mannerich';
+  //String nomeCompleto = 'Lucas Mannerich';
+  dynamic nomeCompleto = 'Lucas Mannerich';
+  nomeCompleto = 1.0;
+  nomeCompleto = true;
+
+  print(nomeCompleto);
+}
