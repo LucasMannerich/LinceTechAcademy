5c9818375076f08c00133bbf4fd803bbe6853e36
LucasMannerich <lucasmannerich@gmail.com>
2024-03-20 14:44:21 -0300

    Aprendendo a diferença entre final e const

--- Dart/Modulo_1/final_const.dart
+++ Dart/Modulo_1/final_const.dart
@@ -1,9 +1,9 @@
 void main() {
   //⬇ O valor não pode mudar a sua atribuição, mas você pode gerenciar seus valores
-  final listaDeLinguagens1 = ["Dart", "Java"];
-  listaDeLinguagens1.add("C#");
+  final listaDeLinguagens1 = ["Dart", "C#"];
+  listaDeLinguagens1.add("Java");
   //⬇ O valor "congela" os valores, eles não podem ser modificados, não é possível add ou remover
-  const listaDeLinguagens2 = ["Dart", "Java"];
+  const listaDeLinguagens2 = ["Dart", "C#"];
 
   print(listaDeLinguagens1);
   print(listaDeLinguagens2);
