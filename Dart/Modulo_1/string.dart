ffc9aa537ec30c68cab4be4fb8e3911049404b61
LucasMannerich <lucasmannerich@gmail.com>
2024-03-20 15:50:32 -0300

    Introdução ao Dart

--- Dart/Modulo_1/string.dart
+++ Dart/Modulo_1/string.dart
@@ -1,16 +1,16 @@
 void main() {
-  String string1 = "Dart";
-  String string2 = 'Dart';
-  String string3 = '''
-  ...
-  Linguagem
-  Dart
-  ...
-  ''';
-  String string4 = """ linguagem Dart """;
-  String string5 = 'It\'s great';
-  String string6 = "It's great";
-  String string7 = 'O meu nome é "Washington"';
+  //String string1 = "Dart";
+  //String string2 = 'Dart';
+  //String string3 = '''
+  //...
+  //Linguagem
+  //Dart
+  //...
+  //''';
+  //String string4 = """ linguagem Dart """;
+  //String string5 = 'It\'s great';
+  //String string6 = "It's great";
+  //String string7 = 'O meu nome é "Washington"';
 
   String linguagem = "Dart";
   String mensagem = "estou estudando a Linguagem $linguagem";
