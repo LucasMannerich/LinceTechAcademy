267d33affe88613bc555b624a571f21ab603d070
LucasMannerich <lucasmannerich@gmail.com>
2024-03-20 15:34:25 -0300

    Trabalhando com texto

---
+++ Dart/Modulo_1/string.dart
@@ -0,0 +1,19 @@
+void main() {
+  String string1 = "Dart";
+  String string2 = 'Dart';
+  String string3 = '''
+  ...
+  Linguagem
+  Dart
+  ...
+  ''';
+  String string4 = """ linguagem Dart """;
+  String string5 = 'It\'s great';
+  String string6 = "It's great";
+  String string7 = 'O meu nome é "Washington"';
+
+  String linguagem = "Dart";
+  String mensagem = "estou estudando a Linguagem $linguagem";
+
+  print(mensagem);
+}
