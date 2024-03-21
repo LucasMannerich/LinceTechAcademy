2ab3992e74659fde4ad36b490a5de92491b77cad
LucasMannerich <lucasmannerich@gmail.com>
2024-03-20 21:55:58 -0300

    Iverter valores

---
+++ Dart/ap1_variaveis/ap3.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+void main() {
+  final random = Random();
+  int numAleatorio1 = random.nextInt(100);
+  int numAleatorio2 = random.nextInt(100);
+
+  print('Foram sorteados os seguintes números: $numAleatorio1 e $numAleatorio2');
+
+  final inversao = numAleatorio1;
+  numAleatorio1 = numAleatorio2;
+  numAleatorio2 = inversao;
+
+  print('Após a inversão os números ficaram na seguinte ordem: $numAleatorio1 e $numAleatorio2');
+  //Não entendi muito bem onde usar const, então optei por usar apenas final
+}
