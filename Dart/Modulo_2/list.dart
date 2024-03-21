28cb1738653afc28e015031a36cbe43870bc9472
LucasMannerich <lucasmannerich@gmail.com>
2024-03-21 09:15:16 -0300

    Curso: Formação Dart

--- Dart/Modulo_2/list.dart
+++ Dart/Modulo_2/list.dart
@@ -1,10 +1,12 @@
 void main() {
   //var lista = [1, 2, 3];
-  bool valorAtivo = false;
+  bool valorAtivo = true;
   List<int> lista1 = [1, 2, 3];
   List<int> lista2 = [4, 5, 6];
+  var lista3 = null;
 
-  var listaTotal = [0, ...lista1, ...lista2, (valorAtivo ? 7 : 8)];
+  // ignore: dead_code
+  var listaTotal = [0, ...lista1, ...lista2, ...?lista3, valorAtivo ? 7 : 8, ];
   // listaTotal.addAll(lista1);
 
   print(listaTotal);
