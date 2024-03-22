1e66382dc4828660803e0f812bf40a892e4b93aa
LucasMannerich <lucasmannerich@gmail.com>
2024-03-22 10:14:44 -0300

    Utilizando métodos de lista #2

---
+++ Dart/ap2_colecoes/ap3.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+void main () {
+  final random = Random();
+  List<int> listaAleatoria = [];
+
+  for(int i = 0; i <= 50; i++) {
+    listaAleatoria.add(random.nextInt(22));
+  }
+
+  listaAleatoria.removeWhere((Number) => (Number < 10));
+  List<int> listaUnica = listaAleatoria.toSet().toList();
+  
+  print('Lista original: ${listaAleatoria}');
+  print('Itens únicos: ${listaUnica}');
+}
