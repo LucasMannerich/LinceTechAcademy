f0d8ff6bdbbe59d44da2858f15df4a0a98ea93d3
LucasMannerich <lucasmannerich@gmail.com>
2024-03-24 20:48:27 -0300

    Operadores aritméticos

---
+++ Dart/Modulo_4/aritmeticos.dart
@@ -0,0 +1,18 @@
+void main() {
+  var numero = 5;
+  print(numero + 2);
+  print(numero - 2);
+  print(numero * 2);
+  print(numero / 2);
+  print(6 % 2); // (1 1) (1 1) (1 1) == 0
+  print(numero ~/ 2); // (1 1) (1 1) 1 == 2
+
+  print(++numero);
+  print(numero);
+  print(--numero);
+  print(numero);
+  print(numero++);
+  print(numero);
+  print(numero--);
+  print(numero);
+}
