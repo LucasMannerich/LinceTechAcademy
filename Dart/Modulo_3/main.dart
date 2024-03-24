cc23fc1ea782b1e31113ea25415b7d4a4377b131
LucasMannerich <lucasmannerich@gmail.com>
2024-03-24 16:32:07 -0300

    Introdução a funções

---
+++ Dart/Modulo_3/main.dart
@@ -0,0 +1,25 @@
+void main() {
+
+  //void Function() minhaFuncao = () {
+  //  print('Minha Função');
+  //};
+
+  //var minhaFuncao = () {
+  //  print('Minha Função');
+  //};
+
+  void minhaFuncao() {
+    print('Minha Função');
+  };
+
+  //num Function(num, num) getSoma = (num v1, num v2) {
+  //  return v1 + v2;
+  //};
+
+  num getSoma (num v1, num v2) {
+    return v1 + v2;
+  };
+
+  minhaFuncao();
+  print(getSoma(2, 3));
+}
