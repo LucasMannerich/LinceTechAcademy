9d4d9dea7d752325964bf1f889b36ffde71b55fb
LucasMannerich <lucasmannerich@gmail.com>
2024-03-24 16:34:35 -0300

    Introdução a funções

--- Dart/Modulo_3/main.dart
+++ Dart/Modulo_3/main.dart
@@ -8,18 +8,15 @@ void main() {
   //  print('Minha Função');
   //};
 
-  void minhaFuncao() {
-    print('Minha Função');
-  };
+  void minhaFuncao() => print('Minha Função');
 
   //num Function(num, num) getSoma = (num v1, num v2) {
   //  return v1 + v2;
   //};
 
-  num getSoma (num v1, num v2) {
-    return v1 + v2;
-  };
-
-  minhaFuncao();
+  //minhaFuncao();
+  minhaFuncao.call();
   print(getSoma(2, 3));
 }
+
+num getSoma (num v1, num v2) => v1 + v2;
