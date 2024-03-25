5ef792ce23c1d744f91b9613a48f6f0381ddc85f
LucasMannerich <lucasmannerich@gmail.com>
2024-03-25 19:23:31 -0300

    Comando de repetição while

---
+++ Dart/Modulo_5/comando_while.dart
@@ -0,0 +1,13 @@
+void main() {
+  var indice = 0;
+
+  //while (indice <= 10) {
+  //  print('Dart $indice');
+  //  indice++;
+  //}
+
+  do {
+    print('Dart $indice');
+    indice++;
+  }while (indice <= 10);
+}
