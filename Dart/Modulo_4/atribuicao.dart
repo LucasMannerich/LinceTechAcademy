6943b06a700440a3c643cfe74fb6d82e2b6265bb
LucasMannerich <lucasmannerich@gmail.com>
2024-03-24 22:24:12 -0300

    Operadores de atribuição

---
+++ Dart/Modulo_4/atribuicao.dart
@@ -0,0 +1,21 @@
+void main() {
+  // =
+  // +=
+  // -=
+  // *=
+  // /=
+  // %=
+  // ~/=
+  // ??=
+
+  var numero = getNumero();
+
+  // numero = numero + 10;
+  // numero += 10;
+
+  numero ??= 50;
+
+  print(numero);
+}
+
+int? getNumero() => null;
