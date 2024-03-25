01959a7aa32029aa8234a13746cd4e82cd297bdf
LucasMannerich <lucasmannerich@gmail.com>
2024-03-25 16:52:12 -0300

    Remover da lista

---
+++ Dart/ap4_operadores/ap3.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+void main () {
+
+  List<int>? listaNumeros = [1, 2, 3, 4, 5];
+
+  int? numeroEscolhido = Random().nextInt(5) + 1;
+
+  List<int> listaModificada = removeNumero(listaNumeros, numeroEscolhido);
+
+  print('Lista modificada: $listaModificada');
+}
+
+List<int> removeNumero<int>(List<int>? listaNumeros, [int? numeroEscolhido]) {
+  listaNumeros ??= [];
+  listaNumeros.remove(numeroEscolhido);
+  return listaNumeros;
+}
