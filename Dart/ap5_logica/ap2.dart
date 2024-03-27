7c5109431ce22d697af5d5f1e1e7e33e133322c4
LucasMannerich <lucasmannerich@gmail.com>
2024-03-27 07:45:18 -0300

    Sequência do alfabeto

---
+++ Dart/ap5_logica/ap2.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+void main() {
+  String numeroParaLetra(int numero) {
+
+    return String.fromCharCode('A'.codeUnitAt(0) + numero - 1);
+  }
+
+  int geradorDeNumeros() {
+    return Random().nextInt(26) + 1;
+  }
+
+  for (int i = 0; i < 5; i++) {
+    int numero = geradorDeNumeros();
+    String letra = numeroParaLetra(numero);
+    print('Número $numero -> Letra $letra');
+  }
+}
