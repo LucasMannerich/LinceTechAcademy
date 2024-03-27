ebd8954265c791e6b194581436584ee9528ec853
LucasMannerich <lucasmannerich@gmail.com>
2024-03-27 07:46:11 -0300

    Contar nomes

---
+++ Dart/ap5_logica/ap4.dart
@@ -0,0 +1,36 @@
+void main() {
+  final listaNomes = [
+    "Joao",
+    "Maria",
+    "Pedro",
+    "Maria",
+    "Ana",
+    "Joao",
+    "Maria",
+    "Fernanda",
+    "Carlos",
+    "Maria"
+  ];
+
+  final nome = 'Ana';
+
+  final quantidade = contaNome(listaNomes, nome);
+
+  if (quantidade == 1) {
+    print('O nome $nome foi encontrado 1 vez');
+  } else if (quantidade > 0) {
+    print('O nome $nome foi encontrado $quantidade vezes');
+  } else {
+    print('O nome nao foi encontrado');
+  }
+}
+
+int contaNome(List<String> listaNomes, String nome) {
+  int quantidade = 0;
+  for (String nomeLista in listaNomes) {
+    if (nomeLista == nome) {
+      quantidade++;
+    }
+  }
+  return quantidade;
+}
