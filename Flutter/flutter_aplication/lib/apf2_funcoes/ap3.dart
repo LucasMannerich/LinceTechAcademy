d74e0fbd18237e210363aaac633721066a184782
LucasMannerich <lucasmannerich@gmail.com>
2024-04-14 19:50:02 -0300

    Formatos dinâmicos

---
+++ Flutter/flutter_aplication/lib/apf2_funcoes/ap3.dart
@@ -0,0 +1,75 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MudarForma(),
+    );
+  }
+}
+
+class MudarForma extends StatefulWidget {
+  const MudarForma({super.key});
+
+  @override
+  AlterarForma createState() => AlterarForma();
+}
+
+class AlterarForma extends State<MudarForma> {
+  bool circulo = true;
+  Color corDaForma = Colors.blue;
+
+  void alternarFormato() {
+    setState(() {
+      circulo = !circulo;
+    });
+  }
+
+  void mudarCor() {
+    setState(() {
+      corDaForma = Color(Random().nextInt(0xFFFFFFFF));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Formatos dinâmicos'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: corDaForma,
+                shape: circulo ? BoxShape.circle : BoxShape.rectangle,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: alternarFormato,
+              child: Text(circulo ? 'Alterar para quadrado' : 'Alterar para círculo'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: mudarCor,
+              child: const Text('Cor Aleatória'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
